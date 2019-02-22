b4563498aab299e40fd89acef3d5532aebd22c76
Renae Heinrich <renae.heinrich@c02wx3xejg5j.myob.myobcorp.net>
2019-02-22 11:27:51 +1100

    initial commit

---
+++ src/GameLauncher.java
@@ -0,0 +1,11 @@
+import models.Player;
+import models.GuessGame;
+public class GameLauncher {
+
+    public static void main(String[] args) {
+     //making a new GuessGame object called game
+    GuessGame game = new GuessGame();
+    //calling the method startGame();
+    game.startGame();
+    }
+}
